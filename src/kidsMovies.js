1141e75b89ac84d45d0fb4c6cea5918a707374dd
GrahamLynch <GrahamLynch10@hotmail.com>
2019-04-25 14:44:34 +0100

    Adding kids movies

---
+++ src/kidsMovies.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    pics: [
+      "https://is5-ssl.mzstatic.com/image/thumb/Video117/v4/77/ec/66/77ec66f3-b174-b70f-314b-c3b09d0ec904/pr_source.lsr/268x0w.png"
+    ],
+    name: "Cars",
+  },
+  {
+    pics: [
+      "https://cdn.europosters.eu/image/750/posters/toy-story-woody-buzz-i24759.jpg"
+    ],
+    name: "Toy Story"
+  },
+  {
+    pics: [
+      "https://dur-duweb.newscyclecloud.com/storyimage/DU/20160420/DGO/160429970/AR/0/AR-160429970.jpg"
+    ],
+    name: "Finding Nemo",
+  },
+  {
+    pics: [
+      "https://images-na.ssl-images-amazon.com/images/I/81ETY9fq1YL._SY445_.jpg"
+    ],
+    name: "The Lion King",
+  },
+  {
+    pics: [
+      "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5754/5754803_sa.jpg;maxHeight=640;maxWidth=550"
+    ],
+    name: "Monsters Inc",
+  },
+  {
+    pics: [
+      "https://images-na.ssl-images-amazon.com/images/I/A17PdqkS1UL._RI_.jpg"
+    ],
+    name: "Frozen",
+  },
+  {
+    pics: [
+      "https://is5-ssl.mzstatic.com/image/thumb/Video62/v4/d9/f4/29/d9f429b3-1471-9d22-db24-1b05752e8baa/contsched.apdadsne.lsr/268x0w.jpg"
+    ],
+    name: "Despicable Me",
+  },
+  {
+    pics: [
+      "https://is2-ssl.mzstatic.com/image/thumb/Video122/v4/68/61/c3/6861c3b4-b291-31b7-07f9-df1d2e625d14/pr_source.jpg/268x0w.jpg"
+    ],
+    name: "The Little Mermaid",
+  },
+  {
+    pics: [
+      "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Up_%282009_film%29.jpg/220px-Up_%282009_film%29.jpg"
+    ],
+    name: "Up",
+  },
+  {
+    pics: [
+      "https://images-na.ssl-images-amazon.com/images/I/81IYVAE6Y-L._SY445_.jpg"
+    ],
+    name: "Beauty and the Beast",
+  },
+  {
+    pics: [
+      "https://images-na.ssl-images-amazon.com/images/I/51iA8isg59L._SY445_.jpg"
+    ],
+    name: "Wall-E",
+  },
+  {
+    pics: [
+      "https://ib2.hulu.com/user/v3/artwork/36c01be2-e036-46d9-9da2-522bfd12b54f?base_image_bucket_name=hummus&base_image=44f49709-7c81-4ede-9a86-6a8003c9d135&size=400x600&format=jpeg"
+    ],
+    name: "Shrek",
+  },
+  {
+    pics: [
+      "https://i.pinimg.com/originals/2c/06/3f/2c063f89005616f72ddb065b45fed65d.jpg"
+    ],
+    name: "Ice Age",
+  },
+  {
+    pics: [
+      "https://images-na.ssl-images-amazon.com/images/I/81opKw7oHQL._SY445_.jpg"
+    ],
+    name: "Ratatouille",
+  },
+  {
+    pics: [
+      "https://upload.wikimedia.org/wikipedia/en/d/df/Harry_Potter_and_the_Deathly_Hallows_%E2%80%93_Part_2.jpg"
+    ],
+    name: "Harry Potter",
+  }
+];
