01277c4fbb8f10acf365e6b42fd6a3efb59d44c7
GrahamLynch <GrahamLynch10@hotmail.com>
2019-04-26 10:40:16 +0100

    Adding kids movies

--- src/kidsMovies.js
+++ src/kidsMovies.js
@@ -4,89 +4,152 @@ export default [
       "https://is5-ssl.mzstatic.com/image/thumb/Video117/v4/77/ec/66/77ec66f3-b174-b70f-314b-c3b09d0ec904/pr_source.lsr/268x0w.png"
     ],
     name: "Cars",
+    genre: [
+      "Adventure",
+      "Sports"
+    ]
   },
   {
     pics: [
       "https://cdn.europosters.eu/image/750/posters/toy-story-woody-buzz-i24759.jpg"
     ],
-    name: "Toy Story"
+    name: "Toy Story",
+    genre: [
+      "Adventure",
+      "Comedy"
+    ]
   },
   {
     pics: [
       "https://dur-duweb.newscyclecloud.com/storyimage/DU/20160420/DGO/160429970/AR/0/AR-160429970.jpg"
     ],
     name: "Finding Nemo",
+    genre: [
+      "Animation",
+      "Comedy"
+    ]
   },
   {
     pics: [
       "https://images-na.ssl-images-amazon.com/images/I/81ETY9fq1YL._SY445_.jpg"
     ],
     name: "The Lion King",
+    genre: [
+      "Drama",
+      "Comedy"
+    ]
   },
   {
     pics: [
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5754/5754803_sa.jpg;maxHeight=640;maxWidth=550"
     ],
     name: "Monsters Inc",
+    genre: [
+      "Adventure",
+      "Comedy"
+    ]
   },
   {
     pics: [
       "https://images-na.ssl-images-amazon.com/images/I/A17PdqkS1UL._RI_.jpg"
     ],
     name: "Frozen",
+    genre: [
+      "Adventure",
+      "Comedy"
+    ]
   },
   {
     pics: [
       "https://is5-ssl.mzstatic.com/image/thumb/Video62/v4/d9/f4/29/d9f429b3-1471-9d22-db24-1b05752e8baa/contsched.apdadsne.lsr/268x0w.jpg"
     ],
     name: "Despicable Me",
+    genre: [
+      "Adventure",
+      "Animation",
+      "Comedy"
+    ]
   },
   {
     pics: [
       "https://is2-ssl.mzstatic.com/image/thumb/Video122/v4/68/61/c3/6861c3b4-b291-31b7-07f9-df1d2e625d14/pr_source.jpg/268x0w.jpg"
     ],
     name: "The Little Mermaid",
+    genre: [
+      "Fantasy",
+      "Animation"
+    ]
   },
   {
     pics: [
       "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Up_%282009_film%29.jpg/220px-Up_%282009_film%29.jpg"
     ],
     name: "Up",
+    genre: [
+      "Adventure",
+      "Animation"
+    ]
   },
   {
     pics: [
       "https://images-na.ssl-images-amazon.com/images/I/81IYVAE6Y-L._SY445_.jpg"
     ],
     name: "Beauty and the Beast",
+    genre: [
+      "Adventure",
+      "Family"
+    ]
   },
   {
     pics: [
       "https://images-na.ssl-images-amazon.com/images/I/51iA8isg59L._SY445_.jpg"
     ],
     name: "Wall-E",
+    genre: [
+      "Adventure",
+      "Comedy"
+    ]
   },
   {
     pics: [
       "https://ib2.hulu.com/user/v3/artwork/36c01be2-e036-46d9-9da2-522bfd12b54f?base_image_bucket_name=hummus&base_image=44f49709-7c81-4ede-9a86-6a8003c9d135&size=400x600&format=jpeg"
     ],
     name: "Shrek",
+    genre: [
+      "Adventure",
+      "Animation",
+      "Comedy"
+    ]
   },
   {
     pics: [
       "https://i.pinimg.com/originals/2c/06/3f/2c063f89005616f72ddb065b45fed65d.jpg"
     ],
     name: "Ice Age",
+    genre: [
+      "Adventure",
+      "Animation",
+      "Comedy"
+    ]
   },
   {
     pics: [
       "https://images-na.ssl-images-amazon.com/images/I/81opKw7oHQL._SY445_.jpg"
     ],
     name: "Ratatouille",
+    genre: [
+      "Drama",
+      "Animation",
+      "Comedy"
+    ]
   },
   {
     pics: [
       "https://upload.wikimedia.org/wikipedia/en/d/df/Harry_Potter_and_the_Deathly_Hallows_%E2%80%93_Part_2.jpg"
     ],
     name: "Harry Potter",
+    genre: [
+      "Adventure"
+    ]
   }
 ];
